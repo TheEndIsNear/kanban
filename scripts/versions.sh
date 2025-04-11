e651e2e07321ab3ec98d1dbf2dbfee021f631040
Matt <2172212+TheEndIsNear@users.noreply.github.com>
2025-04-11 14:22:53 -0600

    attempt to push a docker image

---
+++ scripts/versions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x 
+
+version_output=$(cat ./.tool-versions)
+
+ELIXIR_VERSION=$(echo "$version_output"  \
+	         | grep 'elixir' \
+		 | cut -d' ' -f2 \
+		 | cut -d'-' -f1)
+
+ERLANG_VERSION=$(echo "$version_output" | grep 'erlang' | cut -d' ' -f2)
+
+{
+	echo "ELIXIR_VERSION=${ELIXIR_VERSION}";
+	echo "ERLANG_VERSION=${ERLANG_VERSION}";
+} >> "$GITHUB_ENV"
